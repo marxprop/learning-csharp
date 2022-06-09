049337cbfee70e0f07acc69b325d623236aa7df5
Matthew Oke <105202204+marxprop@users.noreply.github.com>
2022-06-09 10:21:27 +0100

    Simple Console Bank Application

---
+++ MaxpropBank/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MaxpropBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Matthew", 500);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+
+            account.MakeWithdrawal(100, DateTime.Now, "Groceries");
+
+            Console.WriteLine("Account Transaction History...");
+            Console.WriteLine(account.GetAccountHistory());
+
+            Console.ReadLine();
+
+        }   
+    }
+}
